10798e0e72b78abf428df8b82cae06ddbd9ef068
MehmetOzberk <mehmetozberkgal@gmail.com>
2025-01-21 16:16:18 +0300

    added a class for vector in a 3D place. Used vector class to create a color class which translates vectors x,y,z coordinates to r,g,b data

--- 3DRenderer/main.cpp
+++ 3DRenderer/main.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
+#include "vec3.h"
+#include "color.h"
 
-using namespace std;
+
 
 int main() {
-
+	
 	int image_width = 256;
 	int image_height = 256;
 
-	cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-	
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	//RENDER
 	for (int j = 0; j < image_height; j++) {
+		std::clog << "\rRemaining lines: " << (image_height - j) << "\n" << std::flush;
 		for (int i = 0; i < image_width; i++){
-			auto r = double(i) / (image_width - 1);
-			auto g = double(j) / (image_height - 1);
-			auto b = 0.0;
-
-			int ir = int(255.999 * r);
-			int ig = int(255.999 * g);
-			int ib = int(255.999 * b);
-
-			cout << ir << ' ' << ig << ' ' << ib << '\n';
+			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+			write_color(std::cout, pixel_color);
 		}
 	}
+	std::clog << "\rDONE.			\n";
 }
