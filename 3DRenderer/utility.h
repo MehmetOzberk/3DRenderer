b57377b351a958216320dbfd9364f5be8e3a39f3
MehmetOzberk <mehmetozberkgal@gmail.com>
2025-01-30 01:18:22 +0300

    Added diffuse material type Dixed shadow acne Transfered my from linear space to gamma space Added different materials(lambertian,metal,dielectric) Added reflection and refraction methods for glass materials Added fuzzines to reflections Camera is now moveable and can zoom in and out. Added defocus blur(depth of field)
    finally a working complete raytracer but there is still a lot to do for turning this into a game engine.

--- 3DRenderer/utility.h
+++ 3DRenderer/utility.h
@@ -20,7 +20,7 @@ inline double degrees_to_radians(double degrees) {
 	return degrees * pi / 180;
 }
 inline double random_double() {
-	return std::rand() / (RAND_MAX + 1);
+	return std::rand() / (RAND_MAX + 1.0);
 }
 inline double random_double(double min, double max) {
 	return min + (max - min) * random_double();
